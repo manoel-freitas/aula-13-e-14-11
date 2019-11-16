7038eec9cbde531c2e272f9af08a4bb7e23d7542
ligiallucca <lilillucca@gmail.com>
2019-11-16 00:25:47 -0300

    deletando funções inúteis

--- scripts/btn.js
+++ scripts/btn.js
@@ -8,10 +8,3 @@ function changeBtnOnmouseout() {
     removeClass(headerButton, "header-btn-onmouseover")
 }
 
-function changeBtnOnModalActions(action) {
-    action(headerButton, "header-btn-onmodal")
-}
-
-/*function changeBtnByWindowClicked() {
-    addClass(headerButton, "header-btn")
-}*/
